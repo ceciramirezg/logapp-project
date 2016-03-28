de1dae482b630268a04ded931b49f139a3ce5cdd
ceciramirezg <ceci.e.ramirezg@gmail.com>
2016-03-27 23:33:54 -0500

    Test logger again

--- LogApplication/LogApplicacionTest/JobLoggerTests.cs
+++ LogApplication/LogApplicacionTest/JobLoggerTests.cs
@@ -15,7 +15,23 @@ namespace LogApplication.Tests
         public void LogMessageTest()
         {
             JobLogger logger = new JobLogger(LogType.TextFile | LogType.Database);
-            logger.LogMessage("mensaje otro tipo test", MessageType.Warning | MessageType.Error);            
+            logger.LogMessage("mensaje otro tipo test", MessageType.Warning | MessageType.Error);
+
+            //JobLogger logger2 = new JobLogger(LogType.Database);
+            //logger2.LogMessage(null, MessageType.Message);
+
+            JobLogger logger3 = new JobLogger(LogType.Database);
+            logger3.LogMessage("mensaje test 3", MessageType.Message);
+
+            JobLogger logger4 = new JobLogger(LogType.TextFile);
+            logger4.LogMessage("mensaje test 4", MessageType.Error | MessageType.Warning);
+
+            //JobLogger logger5 = new JobLogger(LogType.Console | LogType.Database);
+            //logger5.LogMessage("mensaje test 5", MessageType.Message | MessageType.Warning);   
+
+            JobLogger logger6 = new JobLogger(LogType.TextFile | LogType.Database);
+            logger6.LogMessage("mensaje test 6", MessageType.Error);
+
         }
     }
 }
